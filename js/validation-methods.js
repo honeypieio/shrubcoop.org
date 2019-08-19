c56aa8cae936be33571f1b57ef25cdc6604a256d
Ross Hudson <hello@rosshudson.co.uk>
2019-08-19 19:39:21 +0000

    Deploy from Forestry.io

---
+++ js/validation-methods.js
@@ -0,0 +1,209 @@
+/*
+ * jQuery creditcard2 extension for the jQuery Validation plugin (http://plugins.jquery.com/project/validate).
+ * Ported from http://www.braemoor.co.uk/software/creditcard.shtml by John Gardner, with some enhancements.
+ *
+ * Author: Jack Killpatrick
+ * Copyright (c) 2010 iHwy, Inc.
+ *
+ * Version 1.0.1 (1/12/2010)
+ * Tested with jquery 1.2.6, but will probably work with earlier versions.
+ *
+ * History:
+ * 1.0.0 - released 2008-11-17
+ * 1.0.1 - released 2010-01-12 -> updated card prefixes based on data at: http://en.wikipedia.org/wiki/Credit_card_number and added support for LaserCard
+ *
+ * Visit http://www.ihwy.com/labs/jquery-validate-credit-card-extension.aspx for usage information
+ *
+ * Dual licensed under the MIT and GPL licenses:
+ *   http://www.opensource.org/licenses/mit-license.php
+ *   http://www.gnu.org/licenses/gpl.html
+ */
+
+var cardTypes = [];
+cardTypes[0] = {
+  cardName: "Visa",
+  lengths: "13,16",
+  prefixes: "4",
+  checkdigit: true
+};
+cardTypes[1] = {
+  cardName: "MasterCard",
+  lengths: "16",
+  prefixes: "51,52,53,54,55",
+  checkdigit: true
+};
+
+cardTypes[2] = {
+  cardName: "AmEx",
+  lengths: "15",
+  prefixes: "34,37",
+  checkdigit: true
+};
+cardTypes[3] = {
+  cardName: "Discover",
+  lengths: "16",
+  prefixes: "6011,622,64,65",
+  checkdigit: true
+};
+cardTypes[4] = {
+  cardName: "Maestro",
+  lengths: "12,13,14,15,16,18,19",
+  prefixes: "5018,5020,5038,6304,6759,6761",
+  checkdigit: true
+};
+cardTypes[5] = {
+  cardName: "VisaElectron",
+  lengths: "16",
+  prefixes: "417500,4917,4913,4508,4844",
+  checkdigit: true
+};
+
+// Credit card expiration dates.
+
+var validMonths = [];
+var validYears = [];
+
+var currentYear = parseInt(
+  new Date()
+    .getFullYear()
+    .toString()
+    .substr(2, 2)
+);
+
+var currentMonth = parseInt(
+  new Date()
+    .getMonth()
+) + 1;
+
+if(currentMonth < 10){
+  currentMonth = "0" + currentMonth;
+}
+
+for (i = 0; i < 13; i++) {
+  if (i != 0) {
+    validYears.push((currentYear + +i).toString());
+
+    if (i < 10) {
+      validMonths.push(("0" + i).toString());
+    } else {
+      validMonths.push(i.toString());
+    }
+  } else {
+    validYears.push(currentYear.toString());
+  }
+}
+
+function validateCardNumber(cardNumber, cardName) {
+  try {
+    var cardType = -1;
+    for (var i = 0; i < cardTypes.length; i++) {
+      if (cardName.toLowerCase() == cardTypes[i].cardName.toLowerCase()) {
+        cardType = i;
+        break;
+      }
+    }
+    if (cardType == -1) {
+      return false;
+    } // card type not found
+
+    cardNumber = cardNumber.replace(/[\s-]/g, ""); // remove spaces and dashes
+    if (cardNumber.length == 0) {
+      return false;
+    } // no length
+
+    var cardexp = /^[0-9]{13,19}$/;
+    if (!cardexp.exec(cardNumber)) {
+      return false;
+    } // has chars or wrong length
+
+    cardNumber = cardNumber.replace(/\D/g, ""); // strip down to digits
+
+    if (cardTypes[cardType].checkdigit) {
+      var checksum = 0;
+      var mychar = "";
+      var j = 1;
+
+      var calc;
+      for (i = cardNumber.length - 1; i >= 0; i--) {
+        calc = Number(cardNumber.charAt(i)) * j;
+        if (calc > 9) {
+          checksum = checksum + 1;
+          calc = calc - 10;
+        }
+        checksum = checksum + calc;
+        if (j == 1) {
+          j = 2;
+        } else {
+          j = 1;
+        }
+      }
+
+      if (checksum % 10 != 0) {
+        return false;
+      } // not mod10
+    }
+
+    var lengthValid = false;
+    var prefixValid = false;
+    var prefix = [];
+    var lengths = [];
+
+    prefix = cardTypes[cardType].prefixes.split(",");
+    for (i = 0; i < prefix.length; i++) {
+      var exp = new RegExp("^" + prefix[i]);
+      if (exp.test(cardNumber)) prefixValid = true;
+    }
+    if (!prefixValid) {
+      return false;
+    } // invalid prefix
+
+    lengths = cardTypes[cardType].lengths.split(",");
+    for (j = 0; j < lengths.length; j++) {
+      if (cardNumber.length == lengths[j]) lengthValid = true;
+    }
+    if (!lengthValid) {
+      return false;
+    } // wrong length
+
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+
+function validateEmail(email) {
+  var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
+}
+
+function validateExpirationDate(month, year) {
+  try {
+    if (validMonths.includes(month) && validYears.includes(year)) {
+      if (year == currentYear) {
+        if (month >= currentMonth) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return true;
+      }
+    } else {
+      return false;
+    }
+  } catch (err) {
+    return false;
+  }
+}
+
+function validateCVVNumber(cvv) {
+  if (cvv.length == 3) {
+    if (!isNaN(cvv)) {
+      return true;
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
+}
--- js/volunteer-roles.js
+++ js/volunteer-roles.js
@@ -1,179 +1,23 @@
-var roles = [
-  {
-    role_id: "4N1ksSi8Wk",
-    group_id: "WG-108",
-    details: {
-      title: "Newsletter Volunteer",
-      locations: ["At home", "22 Bread Street"],
-      activities: ["Community", "Marketing/PR/Media"],
-      hours_per_week: "4",
-      commitment_length: "Ongoing",
-      experience_gained:
-        "Copyediting,\r\nEmail marketing and analysing engagement data,\r\nContributing to an environmental project",
-      short_description:
-        "We're building a team enthusiastic volunteers to help write and edit our fortnightly email newsletter to promote our events, volunteer opportunities",
-      experience_required:
-        "Good English writing skills.\r\nGraphic design skills desirable but not essential.\r\nWould suit PR & Marketing student or anyone looking for experience"
-    },
-    availability: { wed_e: "on" },
-    dateCreated: "2019-02-13T15:10:25.000Z",
-    public: 1,
-    removed: 0
-  },
-  {
-    role_id: "9Fc5BvX9AP",
-    group_id: "WG-108",
-    details: {
-      title: "PR and Marketing",
-      role_id: "9Fc5BvX9AP",
-      locations: ["At home", "22 Bread Street"],
-      activities: ["Charity shops/Retail", "Marketing/PR/Media"],
-      hours_per_week: "4",
-      commitment_length: "Ongoing",
-      experience_gained:
-        "Copyediting and drafting press releases.\r\nSeeking out novel opportunities to get Edinburgh talking about waste.\r\nDesigning marketing strategies",
-      short_description:
-        "We're building a team enthusiastic volunteers to help promote our events and shop spaces.",
-      experience_required:
-        "Good English writing skills.\r\nPlanning and organising.\r\nGraphic design skills desirable but not essential.\r\nWould suit PR & Marketing student"
-    },
-    availability: { wed_e: "on" },
-    dateCreated: "2019-01-28T00:00:00.000Z",
-    public: 1,
-    removed: 0
-  },
-  {
-    role_id: "GFtWQvY4Gb",
-    group_id: "WG-108",
-    details: {
-      title: "Graphic Designer",
-      role_id: "GFtWQvY4Gb",
-      locations: ["At home", "22 Bread Street"],
-      activities: [
-        "Charity shops/Retail",
-        "Community",
-        "Marketing/PR/Media",
-        "Practical/DIY"
-      ],
-      hours_per_week: "4",
-      commitment_length: "Ongoing",
-      experience_gained:
-        "Developing brand guidelines.\r\nDesign work for posters, flyers, infographics, social media pages, events programmes, & shop displays.",
-      short_description:
-        "We're building a team enthusiastic volunteers to help promote a series of exciting events and our shop spaces.",
-      experience_required:
-        "Good graphic design skills\r\nPhotoshop, Illustrator and InDesign skills desirable. We also use Canva for day to day design work."
-    },
-    availability: { wed_e: "on" },
-    dateCreated: "2019-01-28T00:00:00.000Z",
-    public: 1,
-    removed: 0
-  },
-  {
-    role_id: "hfn2e3HH8W",
-    group_id: "WG-108",
-    details: {
-      title: "Brand Guardian",
-      role_id: "hfn2e3HH8W",
-      locations: ["At home", "22 Bread Street"],
-      activities: "Marketing/PR/Media",
-      hours_per_week: "4",
-      commitment_length: "Ongoing",
-      experience_gained:
-        "Design work for a wide variety of projects across all sorts of different media, both print and digital.",
-      short_description:
-        "The purpose of this role is to keep our promotional materials in line with our organisation's branding guidelines.",
-      experience_required:
-        "Good graphic design skills and knowledge of Adobe Photoshop/Illustrator and Indesign."
-    },
-    availability: { wed_e: "on" },
-    dateCreated: "2019-01-28T00:00:00.000Z",
-    public: 1,
-    removed: 0
-  },
-  {
-    role_id: "kf3rRsmZ7t",
-    group_id: "WG-108",
-    details: {
-      title: "Swapshop Marketing Volunteer",
-      role_id: "kf3rRsmZ7t",
-      locations: ["At home", "22 Bread Street"],
-      activities: ["Charity shops/Retail", "Marketing/PR/Media"],
-      hours_per_week: "4",
-      commitment_length: "Ongoing",
-      experience_gained:
-        "Copyediting and drafting press releases.\r\nDesigning marketing strategies.\r\nSeeking out novel opportunities to get Edinburgh talking about waste.",
-      short_description:
-        "building a team enthusiastic volunteers to help promote our events and Swap Shop.",
-      experience_required:
-        "Good English writing skills.\r\nPlanning and organising.\r\nGraphic design skills desirable but not essential.\r\nWould suit PR & Marketing student."
-    },
-    availability: { wed_e: "on" },
-    dateCreated: "2019-01-28T00:00:00.000Z",
-    public: 1,
-    removed: 0
-  },
-  {
-    role_id: "rPLk7Q5-L2",
-    group_id: "WG-108",
-    details: {
-      title: "Social Media Volunteer",
-      locations: ["At home", "22 Bread Street"],
-      activities: "Marketing/PR/Media",
-      hours_per_week: "4",
-      commitment_length: "Ongoing",
-      experience_gained:
-        "Contributing to a social media strategy.\r\nWorking with a team to manage content across Facebook, Twitter or Instagram.\r\nMonitoring engagement data.",
-      short_description:
-        "We’re looking for enthusiastic volunteers to run our social media pages to help us promote our events and sharing lots of zero waste content",
-      experience_required:
-        "Good English writing skills.\r\nPlanning and organising.\r\nGraphic design skills desirable but not essential.\r\nWould suit PR & Marketing student."
-    },
-    availability: { wed_e: "on" },
-    dateCreated: "2019-02-13T16:57:43.000Z",
-    public: 1,
-    removed: 0
-  },
-  {
-    role_id: "UwnR82SEtw",
-    group_id: "WG-108",
-    details: {
-      title: "Instagram Guardian",
-      role_id: "UwnR82SEtw",
-      locations: ["At home", "22 Bread Street"],
-      activities: ["Events", "Campaigning/Lobbying", "Marketing/PR/Media"],
-      hours_per_week: "4",
-      commitment_length: "Ongoing",
-      experience_gained:
-        "Contributing to a social media strategy.\r\nWorking with a team to manage content across Facebook, Twitter or Instagram\r\nMonitoring engagement data.",
-      short_description:
-        "We’re looking for enthusiastic volunteers to run our social media pages to help us promote our events and sharing lots of zero waste content.",
-      experience_required:
-        "Good English writing skills.\r\nPlanning and organising.\r\nGraphic design skills desirable but not essential.\r\nWould suit PR & Marketing student"
-    },
-    availability: { wed_e: "on" },
-    dateCreated: "2019-01-28T00:00:00.000Z",
-    public: 1,
-    removed: 0
-  }
-];
+var workingGroups = {};
 
 // Retrieves roles from murakami.
 function getAllVolunteerRoles() {
   // Make request to murakami.
   $.ajax({
     url:
-      "https://murakami.org.uk/api/get/volunteers/roles/get-public?key=" + murakamiRolesKey,
+      "https://murakami.org.uk/api/get/volunteers/roles/get-public?key=" +
+      murakamiRolesKey,
     type: "GET",
     success: function(res) {
       try {
-        if(res.status == "ok" && res.roles.length > 0){
+        if (res.status == "ok" && res.roles.length > 0) {
+          workingGroups = res.workingGroups;
           writeVolunteerRolesList(res.roles);
         } else {
-          writeErrorMessage();
+          writeErrorMessage("volunteerRoles");
         }
       } catch (err) {
-        writeErrorMessage();
+        writeErrorMessage("volunteerRoles");
       }
     }
   });
@@ -192,15 +36,32 @@ function writeVolunteerRolesList(roles) {
 
     var roleTitle = document.createElement("h4");
     roleTitle.innerText = roles[i].details.title;
+    roleTitle.className += "text-shrub mb-0";
+
+    var roleCreated = document.createElement("p");
+    roleCreated.className += "text-muted mb-0";
+    roleCreated.innerText = moment(roles[i].dateCreated).format("Do MMMM YYYY");
 
     var roleTagline = document.createElement("p");
-    roleTagline.innerText = roles[i].group_id;
+    roleTagline.className += "mb-1";
+
+    if (workingGroups[roles[i].group_id]) {
+      roleTagline.innerText = workingGroups[roles[i].group_id].name;
+    } else {
+      roleTagline.innerText = "SHRUB-wide";
+    }
+
+    roleTagline.innerText += " / " + roles[i].details.commitment_length;
+    roleTagline.innerText +=
+      " / " + roles[i].details.hours_per_week + " Hours Per Week";
 
     var roleLink = document.createElement("a");
     roleLink.href = BaseURL + "volunteer-roles/view?roleId=" + roles[i].role_id;
     roleLink.innerText = "Full details";
+    roleLink.className += "link-blue";
 
     roleDiv.appendChild(roleTitle);
+    roleDiv.appendChild(roleCreated);
     roleDiv.appendChild(roleTagline);
     roleDiv.appendChild(roleLink);
 
@@ -211,49 +72,86 @@ function writeVolunteerRolesList(roles) {
 function getVolunteerRole(roleId) {
   $.ajax({
     url:
-      "https://murakami.org.uk/api/get/volunteers/roles/get-public/" + roleId + "?key=" + murakamiRolesKey,
+      "https://murakami.org.uk/api/get/volunteers/roles/get-public/" +
+      roleId +
+      "?key=" +
+      murakamiRolesKey,
     type: "GET",
     success: function(res) {
-      try {
-        if(res.status == "ok" && res.role[0]){
-          writeFullVolunteerRole(res.role[0]);
-        } else {
-          writeErrorMessage();
-        }
-      } catch (err) {
-        writeErrorMessage();
+      if (res.status == "ok" && res.role) {
+        workingGroups = res.workingGroups;
+        writeFullVolunteerRole(res.role);
+      } else {
+        writeErrorMessage("volunteerRole");
       }
     }
   });
 }
 
 function writeFullVolunteerRole(role) {
-  var table = document.createElement("table");
-  table.className = "table";
+  var roleDiv = document.createElement("div");
+  roleDiv.className = "mb-3 pb-3";
+
+  if(workingGroups[role.group_id]){
+    role.details.working_group = workingGroups[role.group_id].name;
+  } else {
+    role.details.working_group = "SHRUB-wide"
+  }
+
+
+
   Object.keys(role.details).forEach(function(key) {
     if (key == "title") {
       window.document.title = role.details.title + window.document.title;
       document.getElementById("roleTitle").innerText = role.details.title;
     } else {
-      var row = document.createElement("tr");
-      var name = document.createElement("td");
-      name.innerText = key.toProperCase();
-      var detail = document.createElement("td");
-      detail.innerHTML = role.details[key];
+      var row = document.createElement("div");
+      row.className = "row mt-3";
+      row.style.borderBottom = "1px #EFEFEF solid"
 
-      row.appendChild(name);
-      row.appendChild(detail);
-      table.appendChild(row);
+      var nameColumn = document.createElement("div");
+      nameColumn.className = "col-md-4";
+
+      var name = document.createElement("p");
+      name.className = "font-weight-bold"
+      name.innerText = key.toProperCase();
+
+      nameColumn.appendChild(name);
+
+      var detailColumn = document.createElement("div");
+      detailColumn.className = "col-md-8";
+
+      var detail = document.createElement("p");
+
+      if(!Array.isArray(role.details[key])){
+        detail.innerHTML = role.details[key];
+      } else {
+        detail.innerHTML = role.details[key].join(", ");
+      }
+
+
+
+
+      detailColumn.appendChild(detail);
+
+      row.appendChild(nameColumn);
+      row.appendChild(detailColumn);
+      roleDiv.appendChild(row);
     }
   });
-  document.getElementById("volunteerRole").appendChild(table);
+
+  document.getElementById("volunteerRole").appendChild(roleDiv);
 }
 
-function writeErrorMessage() {
-  document.getElementById("volunteerRole").innerHTML =
+function writeErrorMessage(id) {
+  document.getElementById(id).innerHTML =
     "<p class='text-muted text-center mx-auto'>No roles to show right now. Check back soon!</p>";
 }
 
-String.prototype.toProperCase = function () {
-  return this.split("_").join(" ").replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+String.prototype.toProperCase = function() {
+  return this.split("_")
+    .join(" ")
+    .replace(/\w\S*/g, function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
 };
